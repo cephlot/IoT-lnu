c07450774af289ed53e72483f06052e81355b5b8
cephlot <c18oln@cs.umu.se>
2022-06-29 17:58:06 +0200

    [update] Adds wifi disconnect function
    Also increases the sleep time to 15 minutes

--- py/boot.py
+++ py/boot.py
@@ -2,8 +2,11 @@ from machine import ADC
 import network, socket, time, secrets, ubinascii, machine
 from umqttsimple import MQTTClient
 
-def do_connect():
+def connect_wifi():
     """Connects to a WiFi network with specified SSID
+
+    :return: WLAN object
+    :rtype: WLAN object
     """
     SSID=secrets.SSID # Network SSID
     KEY=secrets.KEY  # Network key
@@ -17,6 +20,20 @@ def do_connect():
 
     # We should have a valid IP now via DHCP
     print("Wi-Fi Connected ", wlan.ifconfig())
+
+    return wlan
+
+def disconnect_wifi(wlan):
+    """Disconnects to the currently connected WiFi network and disables the WLAN
+    module.
+
+    :param wlan: WLAN object
+    :type wlan: WLAN object
+    """
+    wlan.disconnect()
+    wlan.active(False)
+
+    print("Wi-Fi Disconnected")
 
 def connect_mqtt():
     """Connects to an MQTT broker
@@ -40,9 +57,6 @@ def restart_and_reconnect():
     time.sleep(10)
     machine.reset()
 
-# Connect to WiFi specified in 'secrets.py'
-do_connect()
-
 # Create an ADC object
 adc = ADC(26)
 
@@ -56,29 +70,36 @@ TOPIC = b'rp2040/mcp9700/temperature'
 client_id = ubinascii.hexlify(machine.unique_id())
 mqtt_server = secrets.MQTT
 
-# Create MQTT client connection
-try:
-  client = connect_mqtt()
-except OSError as e:
-  restart_and_reconnect()
-
 while True:
+    # Connect to WiFi specified in 'secrets.py'
+    wlan = connect_wifi()
+
+    # Create MQTT client connection
+    try:
+      client = connect_mqtt()
+    except OSError as e:
+      restart_and_reconnect()
+
     sum = 0
 
     # Collect samples
-    for i in range(0,10):
+    for i in range(0,100):
         voltage = adc.read_u16()*CONV
         sum += (voltage - 0.5)/0.01
 
     # Take the mean of the samples
-    temp = sum/10.0
+    temp = sum/100.0
     print(temp)
 
     try:
+        # Publish the temperature measurement to the MQTT server
         client.publish(TOPIC, (b'{0:3.1f}'.format(temp)))
     except OSError as e:
-        print('Error thrown')
+        # Reset the MCU if error occurs
         restart_and_reconnect()
 
-    time.sleep(10)
+    # Turn off WiFi module
+    disconnect_wifi(wlan)
 
+    # Sleep for 15 minutes.
+    time.sleep(15*60)
